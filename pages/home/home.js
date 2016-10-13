e74081886c3b7666fa91979270d9ea46c4d426bd
coderk <363959579@qq.com>
2016-10-13 15:43:09 +0800

    home页完成

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,15 +1,14 @@
 var proxy = require("../../utils/proxy")
 
-var limit = 20;     //单页帖子显示数量
-
 Page({
   data:{
-    topics: null,
-    page: 1,
-    tab: null
+    topics: null,   //帖子数据数组
+    page: 1,        //当前页
+    tab: "all",      //主题：all|good|share|ask|job
+    limit: 30       //单页帖子显示数量
   },
   onShow:function(){
-    this.getTopics(null, 1);
+    this.getTopics(this.data.tab, this.data.page);
   },
   changeTab: function(e){
       this.getTopics(e.target.id, 1);
@@ -22,14 +21,33 @@ Page({
       this.getTopics(this.data.tab, this.data.page + 1);
   },
   getTopics: function(tab, page){
-      proxy.getTopics(tab, page, limit, (err, data)=>{
+      proxy.getTopics(tab, page, this.data.limit, (err, data)=>{
           if(!err){
+              var now = Date.now();
+              data.data.length && data.data.every((item)=>{
+                  //处理头像
+                  item.author.avatar_url = item.author.avatar_url.replace("//", "");
+                  //处理最后评论时间
+                  var replyTime = new Date(item.last_reply_at).getTime();
+                  item.last_reply_at = transLastReplyTime2String((now - replyTime)/1000);
+                  return true;
+              });
               this.setData({
                   "tab": tab,
                   "page": page,
                   "topics": data.data
-              })
+              });
           }
       });
   }
 })
+
+function transLastReplyTime2String(seconds){
+    if(seconds >= 3600 * 24)
+        return Math.floor(seconds / (3600 * 24)) + "天前";
+    if(seconds >= 3600)
+        return Math.floor(seconds / 3600) + "小时前";
+    if(seconds >= 60)
+        return Math.floor(seconds / 60) + "分钟前";
+    return Math.floor(seconds) + "秒前";
+}
--- utils/proxy.js
+++ utils/proxy.js
@@ -2,15 +2,28 @@ var https = require("./https");
 var ServerURL = "https://cnodejs.org/api/v1";
 
 var topics = "/topics";
+var topic = "/topic"
 
 module.exports = {
     getTopics: function(tab, page, limit, complete){
         var params = {
             limit: limit,
-            page: page
+            page: page,
+            mdrender: false
         }
         if(tab)
             params.tab = tab;
-        https.get(ServerURL+topics, complete, params)
+        httpsGet(topics, complete, params);
+    },
+    getTopic: function(id, complete){
+        httpsGet(ServerURL+topic+"/"+id, complete, {mdrender:false});
     }
 };
+
+function httpsGet(api, complete, params=null){
+    https.get(ServerURL + api, complete, params)
+}
+
+function httpsPost(api, complete, params=null){
+    https.post(ServerURL + api, complete, params);
+}
